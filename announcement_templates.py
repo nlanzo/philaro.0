3387d01f24f9523e6dad475493ed1c55045ee45a
Nick Lanzo <nlanzo@gmail.com>
2026-01-06 18:39:35 -0600

    remove timestamp from big santa 15 min announcement

--- announcement_templates.py
+++ announcement_templates.py
@@ -5,7 +5,7 @@ from constants import SEASONAL_EVENT_ROLE_NAME
 # Message templates for 15-minute advance announcements
 # Use {role} for role mention and {timestamp} for Discord timestamp
 ANNOUNCEMENT_TEMPLATES = {
-    "big_santa": "{role} Big Santa will spawn in 15 minutes! (at {timestamp})",
+    "big_santa": "{role} Big Santa will spawn in 15 minutes!",
     # Add more event types here as needed,
 }
 
